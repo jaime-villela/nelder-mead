2b85f10401e6484895f183de4d2ab249f4ce97f3
Jaime Villela <jaime.villela@gmail.com>
2025-08-29 09:31:02 -0500

    Added functions to replace row at an index.

--- simplex_matrix.py
+++ simplex_matrix.py
@@ -1,6 +1,19 @@
+import numpy as np
+
 class SimplexMatrix:
     """
-    A class to symbolize the simplex in the Nelder Mead algorithm.
+    A class to symbolize the simplex in the Nelder Mead algorithm.  The method uses the concept of a simplex, 
+    which is a special polytope of n + 1 vertices in n dimensions. Examples of simplices include a line segment 
+    in one-dimensional space, a triangle in two-dimensional space, a tetrahedron in three-dimensional space, 
+    and so forth.
+
+    For this initial example, I am tackling a funciton in two dimensions which means the simplex will be a 
+    triangle.
+
+    I'm also taking the liberty of adding a column to the matrix for sorting purposes.  I want to sort the 
+    matrix according to the value of the target function at each row.  The WORST row will be the one where
+    the value of the function at the values of that row is the greatest.  That is, the values of x,y that
+    result in the lowest value of f(x,y) are labeled the BEST row (x,y pair for this example).
 
     Attributes:
         num_rows (int): number of rows in the matrix
@@ -17,16 +30,20 @@ class SimplexMatrix:
         """
         self.num_rows = num_rows
         self.num_cols = num_cols
-
-    def get_dimensions(self):
+        self.matrix = np.zeros((num_rows, num_cols+1))
+    
+    def replace_row_at_index(self, index, new_row):
         """
-        Simple call to get the matrix dimensions
+        Replace a row in the matrix at the specified index.
 
         Args:
-            self (obj): the class itself
+            index (int): The index of the row to replace.
+            new_row (list or np.ndarray): The new row to insert.
 
-        Returns:
-            tupple: a combination of the rows and columns in the matrix
+        Raises:
+            ValueError: If the length of the new row does not match the number of columns.
         """
-        # Method implementation here
-        return (self.num_rows, self.num_cols)
+        if len(new_row) != self.num_cols + 1:
+            raise ValueError("The length of the new row must match the number of columns in the matrix.")
+    
+        self.matrix[index] = new_row
--- simplex_matrix_unit_test.py
+++ simplex_matrix_unit_test.py
@@ -9,14 +9,37 @@ class TestSimplexMatrix(unittest.TestCase):
         simplex = SimplexMatrix(num_rows, num_cols)
         self.assertEqual(simplex.num_rows, num_rows)
         self.assertEqual(simplex.num_cols, num_cols)
+        self.assertEqual(simplex.matrix.shape, (num_rows, num_cols+1))
+        expected_matrix = [[0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0]]
+        self.assertEqual(simplex.matrix.tolist(), expected_matrix)
 
-    def test_get_dimensions(self):
-        """Test the get_dimensions method."""
+    def test_replace_row_invalid_length(self):
+        """Test replacing a row with an invalid length."""
         num_rows = 3
         num_cols = 4
         simplex = SimplexMatrix(num_rows, num_cols)
-        dimensions = simplex.get_dimensions()
-        self.assertEqual(dimensions, (num_rows, num_cols))
+        
+        # Attempt to replace a row with an invalid length
+        new_row = [1, 2, 3]  # Invalid length
+        with self.assertRaises(ValueError):
+            simplex.replace_row_at_index(0, new_row)
+
+    def test_replace_row(self):
+        """Test replacing a row."""
+        num_rows = 3
+        num_cols = 4
+        simplex = SimplexMatrix(num_rows, num_cols)
+        
+        # Attempt to replace a row with an invalid length
+        new_row = [1, 2, 3, 4, 5]
+        simplex.replace_row_at_index(0, new_row)
+        expected_matrix = [[1, 2, 3, 4, 5],
+                           [0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0]]
+
+        self.assertEqual(simplex.matrix.tolist(), expected_matrix)
 
 if __name__ == "__main__":
     unittest.main()
